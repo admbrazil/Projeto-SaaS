2e32b2d22e030839001be63bdae6a9a491ceb77d
Andre Moraes <andre.morraes@gmail.com>
2026-06-12 13:52:13 -0300

    feat: login na raiz /

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
+    path("", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("magic-link/<str:token>/", views.magic_link_login, name="magic_link"),
 ]
