b5f670e221f37b631c9baf7fc512cb30eff47f28
Andre Moraes <andre.morraes@gmail.com>
2026-06-12 13:54:42 -0300

    feat: view dashboard

---
+++ apps/clinics/views.py
@@ -0,0 +1,8 @@
+"""Views do painel da clinica."""
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+@login_required
+def dashboard(request):
+    return render(request, "clinics/dashboard.html")
