97fe7f0ae547662276c5f4cd51e0919286cde213
Andre Moraes <andre.morraes@gmail.com>
2026-06-12 13:52:59 -0300

    fix: adiciona collectstatic

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python manage.py migrate --noinput || true
+python manage.py collectstatic --noinput || true
 python manage.py createsuperuser --noinput || true
 exec gunicorn config.wsgi:application --bind "0.0.0.0:${PORT:-8080}" --workers 2 --timeout 120
