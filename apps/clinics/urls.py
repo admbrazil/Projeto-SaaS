ac50744b5705cf7f66f5078ce9e76a9996687a58
Andre Moraes <andre.morraes@gmail.com>
2026-06-12 14:13:19 -0300

    feat: rotas pacientes/medicos/consultas

--- apps/clinics/urls.py
+++ apps/clinics/urls.py
@@ -5,4 +5,7 @@ app_name = "clinics"
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("pacientes/", views.pacientes_list, name="pacientes"),
+    path("medicos/", views.medicos_list, name="medicos"),
+    path("consultas/", views.consultas_list, name="consultas"),
 ]
